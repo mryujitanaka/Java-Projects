fbbaebd0339606f2111a5dcfe3784198f31c7a94
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-06 01:11:56 -0300

    Update 1_HelloWorld

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/1_HelloWorld/src/HelloWorld.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/1_HelloWorld/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, Yuji!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }
