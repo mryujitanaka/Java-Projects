fe5cac2887ae49bfd07f46ade52d5c4b955de7f4
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-13 15:52:37 -0300

    Update 3_FloatDouble

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/3_FloatDouble/src/yujisremarks/FloatDouble.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/3_FloatDouble/src/yujisremarks/FloatDouble.java
@@ -1,5 +1,16 @@
 package yujisremarks;
 public class FloatDouble {
     public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        System.out.println();
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
     }
 }
