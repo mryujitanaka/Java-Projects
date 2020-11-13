4a88d339a6d16bf047e5489856dce6ccf920fd9f
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-13 16:19:02 -0300

    Update 3_FloatDouble

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/3_FloatDouble/src/yujisremarks/FloatDouble.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/3_FloatDouble/src/yujisremarks/FloatDouble.java
@@ -1,6 +1,8 @@
 package yujisremarks;
 public class FloatDouble {
     public static void main(String[] args) {
+        // Minimum and Maximum Float numbers that can be stored in memory
+        // Size: occupies 32 bits amount of memory, width of 32
         float myMinFloatValue = Float.MIN_VALUE;
         float myMaxFloatValue = Float.MAX_VALUE;
         System.out.println("Float Minimum Value = " + myMinFloatValue);
@@ -8,6 +10,8 @@ public class FloatDouble {
 
         System.out.println();
 
+        // Minimum and Maximum Double numbers that can be stored in memory
+        // Size: occupies 64 bits amount of memory, width of 64
         double myMinDoubleValue = Double.MIN_VALUE;
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double Minimum Value = " + myMinDoubleValue);
