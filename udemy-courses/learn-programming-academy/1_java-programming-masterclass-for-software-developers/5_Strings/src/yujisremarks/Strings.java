96761566185bd1e6369c09734782c1222cd424fc
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-17 14:10:59 -0300

    Update 5_Strings

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/5_Strings/src/yujisremarks/Strings.java
@@ -0,0 +1,22 @@
+package yujisremarks;
+public class Strings {
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        // String is a datatype, which is not a primitive type. It's actually a Class. It's a sequence of characters.
+        String myStringValue = "This is a string :)";
+        System.out.println(myStringValue);
+
+        System.out.println("myStringValue = " + myStringValue);
+
+        myStringValue = myStringValue + ", and this is more.";
+        System.out.println("myStringValue = " + myStringValue);
+    }
+}
