613eadb8ed104225af73861e397b2d51c2c9f067
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-17 18:10:03 -0300

    Update 6_Operators

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/6_Operators/src/yujisremarks/Operators.java
@@ -0,0 +1,31 @@
+package yujisremarks;
+public class Operators {
+    public static void main(String[] args) {
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+
+        result = result - 1; // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+
+        int previousResult2 = result;
+        System.out.println("previousResult = " + previousResult2);
+
+        result = result * 10; // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        int previousResult3 = result;
+        System.out.println("previousResult = " + previousResult3);
+
+        result = result / 5; // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        int previousResult4 = result;
+        System.out.println("previousResult = " + previousResult4);
+
+        result = result % 3; // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+    }
+}
