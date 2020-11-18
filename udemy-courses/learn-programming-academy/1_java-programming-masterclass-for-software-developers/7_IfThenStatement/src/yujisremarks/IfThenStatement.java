5d8265b05116f5f3ef47104dbdbb5f07ff17a7e4
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-18 19:14:20 -0300

    Update 7_IfThenStatement

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/7_IfThenStatement/src/yujisremarks/IfThenStatement.java
@@ -0,0 +1,10 @@
+package yujisremarks;
+public class IfThenStatement {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of Aliens :(");
+        }
+    }
+}
