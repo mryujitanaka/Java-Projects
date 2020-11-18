c2e00ce02eb97e9e3f1768cb568fd0bc3d320c3f
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-18 20:01:37 -0300

    Update 8_LogicalOperator

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/8_LogicalOperator/src/yujisremarks/LogicalOperator.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/8_LogicalOperator/src/yujisremarks/LogicalOperator.java
@@ -1,10 +1,22 @@
 package yujisremarks;
 public class LogicalOperator {
     public static void main(String[] args) {
-        int topScore = 100;
+        int topScore = 80;
         // != ("is not equal to")
         if (topScore != 100) {
-            System.out.println("You got the high score!");
+            System.out.println("topScore != 100");
+        }
+        if (topScore >= 100) {
+            System.out.println("topScore >= 100");
+        }
+        if (topScore <= 100) {
+            System.out.println("topScore <= 100");
+        }
+        int secondTopScore = 81;
+        // && (Logical AND Operator)
+        // This entire condition "topScore > secondTopScore && topScore < 100" must be TRUE
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("topScore > secondTopScore && topScore < 100");
         }
     }
 }
