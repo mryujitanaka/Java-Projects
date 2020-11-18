d088e6bdc0998f373ea2d01031e1082e4f0ceb23
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-18 19:41:07 -0300

    Update 8_LogicalOperator

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/8_LogicalOperator/src/yujisremarks/LogicalOperator.java
@@ -0,0 +1,10 @@
+package yujisremarks;
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int topScore = 100;
+        // != ("is not equal to")
+        if (topScore != 100) {
+            System.out.println("You got the high score!");
+        }
+    }
+}
