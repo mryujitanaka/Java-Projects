e624041f018992a21980c0411af4497b927e4ef4
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-18 18:44:12 -0300

    Update 6_Operators

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/6_Operators/src/yujisremarks/Operators.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/6_Operators/src/yujisremarks/Operators.java
@@ -27,5 +27,47 @@ public class Operators {
 
         result = result % 3; // the remainder of (4 % 3) = 1
         System.out.println("4 % 3 = " + result);
+
+        int previousResult5 = result;
+        System.out.println("previousResult = " + previousResult5);
+
+        // Abbreviating Operators:
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        int previousResult6 = result;
+        System.out.println("previousResult = " + previousResult6);
+
+        result--; // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        int previousResult7 = result;
+        System.out.println("previousResult = " + previousResult7);
+
+        // result = result + 2;
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult8 = result;
+        System.out.println("previousResult = " + previousResult8);
+
+        // result = result * 10
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        int previousResult9 = result;
+        System.out.println("previousResult = " + previousResult9);
+
+        // result = result / 3
+        result /= 3; // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        int previousResult10 = result;
+        System.out.println("previousResult = " + previousResult10);
+
+        // result = result - 2
+        result -= 2; // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
     }
 }
