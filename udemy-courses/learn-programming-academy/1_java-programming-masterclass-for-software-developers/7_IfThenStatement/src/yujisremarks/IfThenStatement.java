02b36a0dc914ed5474dd03df817b30784e7fb13e
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-18 19:45:42 -0300

    Update 7_IfThenStatement

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/7_IfThenStatement/src/yujisremarks/IfThenStatement.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/7_IfThenStatement/src/yujisremarks/IfThenStatement.java
@@ -2,6 +2,8 @@ package yujisremarks;
 public class IfThenStatement {
     public static void main(String[] args) {
         boolean isAlien = false;
+        // == (testing if the two primitive values are equal)
+        // == (is the variable "isAlien" equal to "false"?)
         if (isAlien == false) {
             System.out.println("It is not an Alien!");
             System.out.println("And I am scared of Aliens :(");
