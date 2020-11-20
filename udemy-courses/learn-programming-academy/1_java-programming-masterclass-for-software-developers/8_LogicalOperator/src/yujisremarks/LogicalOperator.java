fe0aa3a01f2e5a68c50eac927bf08c5e6b2f0e7e
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-20 19:57:25 -0300

    Update 8_LogicalOperator

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/8_LogicalOperator/src/yujisremarks/LogicalOperator.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/8_LogicalOperator/src/yujisremarks/LogicalOperator.java
@@ -21,5 +21,40 @@ public class LogicalOperator {
         if ((topScore > 90) || (secondTopScore <= 90)) {
             System.out.println("topScore > 90) || (secondTopScore <= 90");
         }
+
+        // Assignment Operator VS Equals to Operator:
+        /*
+          int newValue = 50;
+          if (newValue = 50) {
+            System.out.println("This is an error");
+          }
+        */
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+        /*
+          // isCar on line 39 is not going into errors because Java require that the operand must be of type of boolean
+          boolean isCar = false;
+          if (isCar = true) {
+            System.out.println("This is not supposed to happen");
+          }
+        */
+        boolean isCar = false;
+        if (isCar == true) {
+            System.out.println("This is correct");
+        }
+
+        // We can simplify the expressions:
+        boolean isCar2 = false;
+        // "isCar2" is the same as "isCar2 == true"
+        if (isCar2) {
+            System.out.println("This is correct 2");
+        }
+        boolean isCar3 = false;
+        // "!isCar3" is the same as "isCar3 != true" or "isCar3 == false"
+        if (!isCar3) {
+            System.out.println("This is correct 3");
+        }
     }
 }
