13c5e2b1e7014dc3d70cd7845f41699c8be809ae
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-20 21:10:43 -0300

    Update 9_TernaryOperator

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/9_TernaryOperator/src/yujisremarks/TernaryOperator.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/9_TernaryOperator/src/yujisremarks/TernaryOperator.java
@@ -7,12 +7,12 @@ public class TernaryOperator {
         // It's a shortcut of the if-then-else statement.
         boolean isCar = false;
         if (isCar) {
-            System.out.println("This is correct 4");
+            System.out.println("This is false.");
         }
         // The 1st operand (isCar) is the condition that evaluate all conditions to true or false
         // The 2nd operand (true) is the value to assign to "wasCar" if the 1st condition is true
         // The 3rd operand (false) is what to assign to "wasCar" if the  1st operand condition is false
-        isCar = true;
+        isCar = true; // This is true
         boolean wasCar = isCar ? true : false;
         if (wasCar) {
             System.out.println("wasCar is true.");
