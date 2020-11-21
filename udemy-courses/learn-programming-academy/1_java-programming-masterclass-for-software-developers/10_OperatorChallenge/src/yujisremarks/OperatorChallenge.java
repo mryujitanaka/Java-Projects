666926f0c77a9a4a19b7499e17bfce5430af40d5
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-21 17:36:16 -0300

    Update 10_OperatorChallenge

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/10_OperatorChallenge/src/yujisremarks/OperatorChallenge.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/10_OperatorChallenge/src/yujisremarks/OperatorChallenge.java
@@ -5,9 +5,11 @@ public class OperatorChallenge {
         double valueOne = 20.00d;
         double valueTwo = 80.00d;
         double valueThree = (valueOne + valueTwo) * 100.00d;
+        System.out.println("valueThree = " + valueThree);
         double valueFour = valueThree % 40.00d;
+        System.out.println("valueFour = " + valueFour);
         boolean valueFive = (valueFour == 0d) ? true : false;
-        System.out.println(valueFive);
+        System.out.println("valueFive = " + valueFive);
         if (!valueFive) {
             System.out.println("Got some remainder");
         }
