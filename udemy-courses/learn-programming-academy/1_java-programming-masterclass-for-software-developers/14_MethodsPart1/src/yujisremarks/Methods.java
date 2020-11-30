303a29696fcb689eb969d0b4ee75a525d6c78f26
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-30 09:38:48 -0300

    Update 14_MethodsPart1

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/14_MethodsPart1/src/yujisremarks/Methods.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/14_MethodsPart1/src/yujisremarks/Methods.java
@@ -12,10 +12,7 @@ public class Methods {
             System.out.println("Your final score was " + newFinalScore);
         }
         */
-
         calculateScore(true, 800, 5, 100);
-
-        calculateScore(true, 10000, 8, 200);
 
         /*
         score = 10000;
@@ -26,6 +23,7 @@ public class Methods {
             System.out.println("2. Your final score was " + finalScore);
         }
         */
+        calculateScore(true, 10000, 8, 200);
     }
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         /*
