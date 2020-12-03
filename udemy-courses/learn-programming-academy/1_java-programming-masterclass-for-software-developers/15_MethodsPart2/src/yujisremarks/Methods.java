aac448d17d38a277b954e26c8963fd8ee8af8e4f
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-03 12:01:12 -0300

    Update 15_MethodsPart2

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/15_MethodsPart2/src/yujisremarks/Methods.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/15_MethodsPart2/src/yujisremarks/Methods.java
@@ -1,37 +1,22 @@
 package yujisremarks;
 public class Methods {
     public static void main(String[] args) {
+        // We can continue to use statements with the Methods. There is nothing wrong with that. This actually makes
+        // the code easier to read.
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
-        /*
-        if (gameOver) {
-            int newFinalScore = score + (levelCompleted * bonus);
-            newFinalScore += 1000;
-            System.out.println("Your final score was " + newFinalScore);
-        }
-        */
+
         calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        /*
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("2. Your final score was " + finalScore);
-        }
-        */
+
         calculateScore(gameOver, score, levelCompleted, bonus);
     }
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        /*
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-        */
         if (gameOver) {
             int newFinalScore = score + (levelCompleted * bonus);
             newFinalScore += 2000;
