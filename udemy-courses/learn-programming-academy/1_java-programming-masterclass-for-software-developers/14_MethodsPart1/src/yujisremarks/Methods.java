7a34b6a89fe2bbc70e66c33e92b4bb6aee2d8453
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-03 11:23:11 -0300

    Update 14_MethodsPart1

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/14_MethodsPart1/src/yujisremarks/Methods.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/14_MethodsPart1/src/yujisremarks/Methods.java
@@ -1,6 +1,7 @@
 package yujisremarks;
 public class Methods {
     public static void main(String[] args) {
+        // Method
         // A Method is a block of code which only runs when it is called.
         // We can pass data, known as parameters, into a method.
         // Methods are used to perform certain actions, and they are also known as functions.
@@ -17,6 +18,8 @@ public class Methods {
             System.out.println("Your final score was " + newFinalScore);
         }
         */
+
+        // "true", "800", "5" and "100" are Arguments of the Method "calculateScore".
         calculateScore(true, 800, 5, 100);
 
         /*
@@ -28,8 +31,19 @@ public class Methods {
             System.out.println("Your final score was " + finalScore);
         }
         */
+
+        // "true", "10000", "8" and "200" are Arguments of the Method "calculateScore".
         calculateScore(true, 10000, 8, 200);
     }
+    // Parameters and Arguments
+    // Information can be passed to methods as parameter. Parameters act as variables inside the method.
+    // Parameters are specified after the method name, inside the parentheses. We can add as many parameters as we
+    // want, just separate them with a comma.
+    //
+    // When a parameter is passed to the method, it is called an argument.
+    //
+    // So, we can say that the variables declared within a Method (inside the parentheses) are now called Parameters.
+    // And the values of these variables will be called Arguments.
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         /*
         boolean gameOver = true;
