bcda2f1645b14ac6ecc36cc72f65df0c8ae2f342
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-03 12:57:22 -0300

    Update 16_MethodsPart3

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/16_MethodsPart3/src/yujisremarks/Methods.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/16_MethodsPart3/src/yujisremarks/Methods.java
@@ -5,33 +5,30 @@ public class Methods {
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
-        /*
-        if (gameOver) {
-            int newFinalScore = score + (levelCompleted * bonus);
-            newFinalScore += 1000;
-            System.out.println("Your final score was " + newFinalScore);
-        }
-        */
+
         calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        /*
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("2. Your final score was " + finalScore);
-        }
-        */
+
         calculateScore(gameOver, score, levelCompleted, bonus);
     }
+    // Return Values in the Method
+    // The "void" keyword, used in the examples until now, indicates that the method should not return a value. If we
+    // want the method to return a value, we can use a primitive data type (such as "int", "char", etc.) instead of
+    // "void", and use the "return" keyword inside the method.
+    //
+    // Sometimes when we return values in the method, considering this example, when the value of the variable
+    // "gameOver" is true, we need to return a false value, which in this case would be "return -1;", even if there is
+    // not one. Otherwise, we will have errors.
+    //
+    // In this example, we are just learning how to "declare" or "employ" the use of Return in the Method. In this
+    // situation, we are saying that we want to return the value or result of "finalScore". But in reality, we never
+    // return anything.
+    //
+    // "-1" here is used to indicate an error, invalid value, or value not found.
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        /*
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-        */
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
