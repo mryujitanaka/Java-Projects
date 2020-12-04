94fc7884d3366eae984fe0ef5099765b144100f8
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-04 12:24:31 -0300

    Update 18_MethodsChallenge

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/18_MethodsChallenge/src/yujisremarks/MethodsChallenge.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/18_MethodsChallenge/src/yujisremarks/MethodsChallenge.java
@@ -1,27 +1,30 @@
 package yujisremarks;
 public class MethodsChallenge {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int playerPosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Fulano", playerPosition);
 
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
+        playerPosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Beltrano", playerPosition);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        playerPosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Sicrano", playerPosition);
 
-        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
+        playerPosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Fulaninho", playerPosition);
     }
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            return finalScore;
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score table.");
+    }
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else {
+            return 4;
         }
-        return -1;
     }
 }
