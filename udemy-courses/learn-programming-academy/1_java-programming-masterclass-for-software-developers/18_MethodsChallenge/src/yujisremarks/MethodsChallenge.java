c8b8129ec451342da1df8f1628c6f83b57260398
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-07 20:55:49 -0300

    Update 18_MethodsChallenge

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/18_MethodsChallenge/src/yujisremarks/MethodsChallenge.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/18_MethodsChallenge/src/yujisremarks/MethodsChallenge.java
@@ -33,17 +33,17 @@ public class MethodsChallenge {
         playerPosition = calculateHighScorePosition(100);
         displayHighScorePosition("Fulano Jr.", playerPosition);
     }
-    // 1st Method: "displayHighScorePosition"
+    // 1st Method of the challenge: "displayHighScorePosition"
     public static void displayHighScorePosition(String playerName, int playerPosition) {
         System.out.println(playerName + " managed to get into position " + playerPosition + " on the high score table.");
     }
-    // 2nd Method: "calculateHighScorePosition"
+    // 2nd Method of the challenge: "calculateHighScorePosition"
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
         } else {
             return 4;
