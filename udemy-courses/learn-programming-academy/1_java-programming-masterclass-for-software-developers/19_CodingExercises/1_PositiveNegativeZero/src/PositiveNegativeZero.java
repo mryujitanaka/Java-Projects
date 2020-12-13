3339e35dc0de382ff6cdb3113de61f03604f1327
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-12-13 09:28:22 -0300

    Update 1_PositiveNegativeZero

--- udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/19_CodingExercises/1_PositiveNegativeZero/src/PositiveNegativeZero.java
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/19_CodingExercises/1_PositiveNegativeZero/src/PositiveNegativeZero.java
@@ -1,6 +1,8 @@
 public class PositiveNegativeZero {
     public static void main(String[] args) {
         checkNumber(1);
+        checkNumber(-1);
+        checkNumber(0);
     }
     public static void checkNumber(int number) {
         if (number > 0) {
