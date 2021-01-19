b237233f9db1642eb99ceb592dcef652945051bd
Yuji Tanaka <yujitanaka.developer@gmail.com>
2021-01-19 17:17:47 -0300

    Update 3_MegaBytesConverter

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/16_CodingExercises/3_MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,11 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int MB = (kiloBytes / 1024);
+            int remainderKB = (kiloBytes % 1024);
+            System.out.println(kiloBytes + " KB = " + MB + " MB and " + remainderKB + " KB");
+        }
+    }
+}
