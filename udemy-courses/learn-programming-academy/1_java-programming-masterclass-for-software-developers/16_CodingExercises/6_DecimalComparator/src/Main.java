ba8d8e2ed9cd0ca6587f718ba29459b5305c23ce
Yuji Tanaka <yujitanaka.developer@gmail.com>
2021-01-22 10:12:43 -0300

    Update 6_DecimalComparator

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/16_CodingExercises/6_DecimalComparator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean areTheyEquals = DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123);
+        System.out.println(areTheyEquals);
+    }
+}
