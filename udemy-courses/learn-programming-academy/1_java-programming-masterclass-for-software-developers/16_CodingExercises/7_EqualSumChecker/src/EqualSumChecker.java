9fa8487f1a6b1c6b517265a9a2561699db4f8381
Yuji Tanaka <yujitanaka.developer@gmail.com>
2021-01-24 09:30:25 -0300

    Update 7_EqualSumChecker

---
+++ udemy-courses/learn-programming-academy/1_java-programming-masterclass-for-software-developers/16_CodingExercises/7_EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,7 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int v, int y, int t) {
+        if ((v + y) == t) {
+            return true;
+        } return false;
+    }
+}
