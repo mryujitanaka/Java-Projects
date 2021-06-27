070b7bb09def2272daf5af7d9f1c39875bac43f7
Yuji Tanaka <yujitanaka.developer@gmail.com>
2021-06-27 10:47:30 -0300

    Update OlaMundoOpenJFX

--- YouTube-Courses/Curso-em-Video/Aula03.1/OlaMundoOpenJFX/src/olamundoopenjfx/Controller.java
+++ YouTube-Courses/Curso-em-Video/Aula03.1/OlaMundoOpenJFX/src/olamundoopenjfx/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package olamundoopenjfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- YouTube-Courses/Curso-em-Video/Aula03.1/OlaMundoOpenJFX/src/olamundoopenjfx/Main.java
+++ YouTube-Courses/Curso-em-Video/Aula03.1/OlaMundoOpenJFX/src/olamundoopenjfx/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package olamundoopenjfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("OlaMundoOpenJFX.fxml"));
         primaryStage.setTitle("Ola Mundo");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
