a67166087c82bfbc7c02aa02a8fa0998b5d8926c
Yuji Tanaka <yujitanaka.developer@gmail.com>
2024-11-04 17:51:44 -0300

    Update Main.java

--- YouTube-Courses/Bro-Code/1_Java-tutorial-for-beginners/Lesson-02_Variables-in-Java/src/Main.java
+++ YouTube-Courses/Bro-Code/1_Java-tutorial-for-beginners/Lesson-02_Variables-in-Java/src/Main.java
@@ -2,7 +2,44 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, Mr Tanaka!");
+		/*
+		int x; // declaration
+		x = 123; // assignment
+		int x = 123; // initialization
+		*/
+		
+		// Integer Types
+		byte a = -128;
+		byte b = 127;
+		
+		short c = -32768; 
+		short d = 32767;
+		
+		int e = -2147483648;
+		int f = 2147483647;
+		
+		long g = -9223372036854775808L;
+		long h = 9223372036854775807L;
+		
+		// Floating Point Types
+		float i = 3.141592f;
+		
+		double j = 3.14159265358979;
+		
+		// Boolean Values
+		boolean k = true;
+		boolean l = false;
+		
+		// Char Values
+		char m = '@';
+		
+		// Strings
+		String n = "Yoshi";
+		
+		System.out.println("My number is: "+j);
+		System.out.println("Boolean value: " +l);
+		System.out.println("Char value: " +m);
+		System.out.println("String: " +n);
 
 	}
 
