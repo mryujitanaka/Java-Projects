9f565dc51898f94e665e62baaa97280ce87e1dad
Yuji Tanaka <yujitanaka.developer@gmail.com>
2024-11-04 16:48:05 -0300

    Update Java-Projects

---
+++ YouTube-Courses/Bro-Code/1_Java-tutorial-for-beginners/1_Lesson-01_Java-tutorial-for-beginners/HelloWorld/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Mr Tanaka!");
+		System.out.println("Welcome back to the Java world!");
+		
+		//escape character (break lines)
+		System.out.print("It's been a while since you've programmed in Java.\n");
+		System.out.print("It's so good to have you back here!\n");
+		
+		System.out.println("We have a message to you:\n");
+		System.out.println("WELCOME HOME!");
+		
+		//escape character (tabs)
+		System.out.println("\tWELCOME!");
+		
+		//escape character (double quotes)
+		System.out.println("\"WELCOME!\"");
+		
+		//escape character (backslash)
+		System.out.println("\\Welcome!!!");
+		
+		// sysout + Ctrl + Space (shortcut)
+		// System.out.println();
+
+	}
+
+}
