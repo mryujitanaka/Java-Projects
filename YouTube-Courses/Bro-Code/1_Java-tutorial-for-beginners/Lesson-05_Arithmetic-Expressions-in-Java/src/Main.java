bd56fe1cc552188bc66a01f68e47cf8f0e30ed9e
Yuji Tanaka <yujitanaka.developer@gmail.com>
2024-11-08 17:04:49 -0300

    Update Bro-Code

---
+++ YouTube-Courses/Bro-Code/1_Java-tutorial-for-beginners/Lesson-05_Arithmetic-Expressions-in-Java/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Expressions = Operands & Operators
+		// Operands = values, variables, numbers, quantity
+		// Operators = + - * / %
+		
+		// int friends = 10;
+		double friends = 10;
+		
+		// friends = friends + 2;
+		// friends +=2;
+		
+		// friends = friends - 2;
+		// friends -= 2;
+		
+		// friends = friends * 2;
+		// friends *= 2;
+		
+		// friends = friends / 2;
+		// friends /= 5;
+		
+		// friends = friends % 3;
+		// friends %= 3;
+		
+		// friends++;
+		
+		// friends--;
+		
+		friends = friends / 3;
+		
+		System.out.println("Friends = " +friends);
+
+	}
+
+}
