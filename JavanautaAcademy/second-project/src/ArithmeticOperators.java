170cd3588fd82fd35810ecea07163b1325bfce57
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-01 13:16:01 -0300

    Update ArithmeticOperators.java

--- JavanautaAcademy/second-project/src/ArithmeticOperators.java
+++ JavanautaAcademy/second-project/src/ArithmeticOperators.java
@@ -1,5 +1,21 @@
 public class ArithmeticOperators {
     public static void main(String[] args) {
-        System.out.println("Test!");
+        double bread = 10.00;
+        double cheese = 7.50;
+        double coffee = 16.00;
+        double storeDiscount = 5.00;
+        int willSplitBill = 2;
+        int month = 30;
+
+        double totalToPay = bread + cheese + coffee;
+        double totalToPayWithDiscount = totalToPay - storeDiscount;
+        double totalDividing = totalToPayWithDiscount / willSplitBill;
+        double totalAmountSpentInTheMonth = totalToPayWithDiscount * month;
+
+        System.out.println("Total: R$" + totalToPay);
+        System.out.println("Discount: R$" + storeDiscount);
+        System.out.println("Total to pay: R$" + totalToPayWithDiscount);
+        System.out.println("Split the bill in: " + totalDividing);
+        System.out.println("Total amount spent in the store during the month: R$" + totalAmountSpentInTheMonth);
     }
 }
