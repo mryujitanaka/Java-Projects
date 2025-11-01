248321387d2b837599d86a53ed71908e59c71bfa
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-01 10:41:48 -0300

    First project of the Javanauta course

---
+++ JavanautaAcademy/first-project/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Instruction 1: ");
+        System.out.println("Instruction 2: ");
+        System.out.println("Instruction 3: ");
+        System.out.println("Instruction 4: ");
+    }
+}
