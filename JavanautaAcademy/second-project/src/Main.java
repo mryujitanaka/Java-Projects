a033cbb2b9f49cf665e4343ad54a8201f062add3
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-01 12:09:48 -0300

    Second project of the Javanauta course

---
+++ JavanautaAcademy/second-project/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 29;
+        double monthlySalary = 4000.55;
+        char gender = 'M';
+        boolean isEmployed = false;
+
+        System.out.println("Age: " + age);
+        System.out.println("Monthly salary: " + monthlySalary);
+        System.out.println("Gender: " + gender);
+        System.out.println("Employed by any company? " + isEmployed);
+    }
+}
