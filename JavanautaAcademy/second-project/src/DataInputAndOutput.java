5805b148161ec6198e19a247c226bdd70dd5ae94
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-02 09:41:40 -0300

    Create DataInputAndOutput.java

---
+++ JavanautaAcademy/second-project/src/DataInputAndOutput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DataInputAndOutput {
+    public static void main(String[] args) {
+//        System.out.println("Print with line below");
+//        System.out.print("Same-line printing");
+//        System.out.printf("Formatted print");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Type your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Are you employed? Yes (true) or No (false)?");
+        boolean employed = scanner.nextBoolean();
+
+        System.out.println("Name: " + name + ", Age: " + age + ", Employed: " + employed);
+    }
+}
