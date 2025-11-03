fd6fa09114d7274ddcb30d66bdec095f4a91067e
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-03 20:47:17 -0300

    Create ComparativeExpressions.java

---
+++ JavanautaAcademy/second-project/src/ComparativeExpressions.java
@@ -0,0 +1,13 @@
+public class ComparativeExpressions {
+    public static void main(String[] args) {
+        int tapioquinha = 10;
+        int coffee = 20;
+
+        System.out.println(tapioquinha == coffee);
+        System.out.println(tapioquinha != coffee);
+        System.out.println(tapioquinha > coffee);
+        System.out.println(tapioquinha < coffee);
+        System.out.println(tapioquinha >= coffee);
+        System.out.println(tapioquinha <= coffee);
+    }
+}
