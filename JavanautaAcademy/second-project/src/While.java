6b2f2d4beb6cc16fdf44ad588ac456a442d8af65
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-06 20:54:49 -0300

    Create While.java

---
+++ JavanautaAcademy/second-project/src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <= 5) {
+            System.out.println("Counter: " + counter);
+            // counter = counter + 1;
+            counter++;
+        }
+    }
+}
