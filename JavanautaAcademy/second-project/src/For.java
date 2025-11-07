0e3887ed4598002d7d9bedfe1c1fac735dc0dbfa
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-06 22:13:36 -0300

    Create For.java

---
+++ JavanautaAcademy/second-project/src/For.java
@@ -0,0 +1,7 @@
+public class For {
+    public static void main(String[] args) {
+        for (int counter = 0; counter <= 5; counter++) {
+            System.out.println("Counter: " + counter);
+        }
+    }
+}
