c730b95afd3563fe612b882a51aa02e8e5263bcc
Yuji Tanaka <yujitanaka.developer@gmail.com>
2025-11-06 21:04:48 -0300

    Create DoWhile.java

---
+++ JavanautaAcademy/second-project/src/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println("Counter: " + counter);
+            counter++;
+        } while (counter <= 5);
+    }
+}
