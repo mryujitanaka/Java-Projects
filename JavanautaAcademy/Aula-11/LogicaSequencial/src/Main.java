deee3357aea62a49100d2f721d6374ea6174bbe6
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-16 16:40:01 -0300

    Update Aula-11

---
+++ JavanautaAcademy/Aula-11/LogicaSequencial/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Instrução nº 1");
+        System.out.println("Instrução nº 3");
+        System.out.println("Instrução nº 4");
+        System.out.println("Instrução nº 2");
+    }
+}
