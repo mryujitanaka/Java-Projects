81f362559bc66e87b540abce50538720f8a8484f
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-16 17:34:19 -0300

    Create Aula-14

---
+++ JavanautaAcademy/Aula-14/TiposPrimitivos/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 30;
+        long quantidadePets = 2L;
+        double salarioMensal = 3990.90;
+        char genero = 'M';
+        boolean empregadoJava = false;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário mensal: " + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Trabalhando com Java: " + empregadoJava);
+    }
+}
