01fdfe9c45a125bf00c0047b20f2ac87585fc3e1
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-16 16:03:51 -0300

    Update JavanautaAcademy

--- JavanautaAcademy/first-project/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Instruction 1");
-        System.out.println("Instruction 2");
-        System.out.println("Instruction 3");
-        System.out.println("Instruction 4");
-    }
-}
--- JavanautaAcademy/second-project/src/For.java
+++
@@ -1,7 +0,0 @@
-public class For {
-    public static void main(String[] args) {
-        for (int counter = 0; counter <= 5; counter++) {
-            System.out.println("Counter: " + counter);
-        }
-    }
-}
