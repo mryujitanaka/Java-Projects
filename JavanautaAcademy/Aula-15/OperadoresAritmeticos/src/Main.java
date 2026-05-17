753d8c48dcf48a7c9e443baa4fca94f14febc4cf
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-17 18:27:37 -0300

    Create Aula-15

---
+++ JavanautaAcademy/Aula-15/OperadoresAritmeticos/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double pao = 9.50;
+        double queijo = 7.00;
+        double acucar = 1.00;
+
+        double desconto = 5.00;
+
+        int totalDiasMes = 30;
+
+        double valorTotal = pao + queijo + acucar;
+        double valorTotalComDesconto = valorTotal - desconto;
+        double valorTotalDividido = valorTotalComDesconto / 2;
+        double valorTotalMensal = valorTotalComDesconto * totalDiasMes;
+
+        System.out.println("Valor total = R$ " + valorTotalMensal);
+    }
+}
