b7b3ff172fbb6af8cc09f7f195d5a62250856392
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-21 10:15:25 -0300

    Create Aula-20

---
+++ JavanautaAcademy/Aula-20/ExpressoesComparativas/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int pao = 10;
+        int acucar = 20;
+
+        System.out.println("A expressão \"pao == acucar\" é " + (pao == acucar));
+        System.out.println("A expressão \"pao != acucar\" é " + (pao != acucar));
+        System.out.println("A expressão \"pao > acucar\" é " + (pao > acucar));
+        System.out.println("A expressão \"pao < acucar\" é " + (pao < acucar));
+        System.out.println("A expressão \"pao >= acucar\" é " + (pao >= acucar));
+        System.out.println("A expressão \"pao <= acucar\" é " + (pao <= acucar));
+    }
+}
