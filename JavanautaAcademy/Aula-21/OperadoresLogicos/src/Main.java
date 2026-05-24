479d3a1cc773e168662fb2eb0be9df88f0708b0e
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-24 12:45:04 -0300

    Create Aula-21

---
+++ JavanautaAcademy/Aula-21/OperadoresLogicos/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+        System.out.println(x || y);
+        System.out.println(x != y);
+        System.out.println(!x);
+        System.out.println(!y);
+    }
+}
