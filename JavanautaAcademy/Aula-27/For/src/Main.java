7748fd1aab72057efe03de4de6db7ca00fc553b8
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-28 08:37:32 -0300

    Create Aula-27

---
+++ JavanautaAcademy/Aula-27/For/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // int contador = 0;
+
+        /*
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+        */
+
+        /*
+        do {
+            System.out.println("Contador: " + contador);
+            contador++;
+        } while (contador <= 5);
+        */
+
+        for (int contador = 0; contador <= 5; contador++) {
+            System.out.println("Contador: " + contador);
+        }
+    }
+}
