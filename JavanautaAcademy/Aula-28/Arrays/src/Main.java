f610baf7f25159b1ff95faa6263d102a10ad21c8
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-05-29 12:28:30 -0300

    Create Aula-28

---
+++ JavanautaAcademy/Aula-28/Arrays/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        int[] numeros;
+        numeros = new int[] {10, 20, 30, 40, 50, 60};
+        */
+        int[] numeros = {10, 20, 30, 50, 60};
+
+        /*
+        String[] frutas;
+        frutas = new String[] {"Maçã", "Morango", "Abacaxi"};
+        */
+        String[] frutas = {"Maçã", "Morango", "Abacaxi"};
+
+        // Another way to use Array
+        double[] salarios = new double[5];
+        salarios[0] = 150.00;
+        salarios[1] = 200.00;
+        salarios[2] = 25.00;
+        salarios[3] = 500.00;
+        salarios [4] = 100.00;
+
+        // Iteração sobre o Array de salários com for
+        /*
+        for (int posicao = 0; posicao < salarios.length; posicao++){
+            System.out.println("Salário: " + salarios[posicao]);
+        }
+        */
+
+        // Iteração sobre o Array de salários com for-each
+        for (double salario : salarios){
+            System.out.println("Salário: " + salario);
+        }
+
+        // Alteração de valores do Array de salários
+        salarios[2] = 250.00;
+        System.out.println("Salário da posição 2 alterada: " + salarios[2]);
+    }
+}
