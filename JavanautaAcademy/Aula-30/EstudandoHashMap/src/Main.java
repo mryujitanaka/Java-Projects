784aacd487d9068fa223e73420370c74367a0168
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-08-30 14:13:47 -0300

    Update Aula-30

--- JavanautaAcademy/Aula-30/EstudandoHashMap/src/Main.java
+++ JavanautaAcademy/Aula-30/EstudandoHashMap/src/Main.java
@@ -1,3 +1,31 @@
+/*
+The Collections Framework
+
+Before we explore ArrayList, HashSet, HashMap, and other data structures in more detail, it's important to understand that all of these are part of something bigger - the Java Collections Framework.
+
+The Java Collections Framework provides a set of interfaces (like List, Set, and Map) and a set of classes (ArrayList, HashSet, HashMap, etc.) that implement those interfaces.
+
+All of these are part of the java.util package.
+
+They are used to store, search, sort, and organize data more easily - all using standardized methods and patterns.
+
+Tip: Think of the Collections Framework as a toolbox.
+
+Interfaces like Map define what tools can do, and classes like HashMap are the actual tools that do the work.
+*/
+
+/*
+Java Map Interface
+
+The Map interface is a part of the Java Collections Framework and is used to store key-value pairs. Each key must be unique, but values can be duplicated.
+
+A Map is useful when you want to associate a key (like a name or ID) with a value (like an age or description).
+
+Common classes that implement Map:
+
+HashMap - fast and unordered
+*/
+
 import java.util.HashMap; // Import the HashMap class
 import java.util.Map; // Import the Map class
 
