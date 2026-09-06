fcd5957d87e68a6a3953c8016f0cf8aee9594019
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-09-06 16:08:19 -0300

    Update Aula-30.1

---
+++ JavanautaAcademy/Aula-30.1/EstudandoTreeMap/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.TreeMap;
+
+public class Test {
+    public static void main(String[] args) {
+
+        TreeMap<String, Double> sistemaNotasAlunos = new TreeMap<>();
+
+        sistemaNotasAlunos.put("Yoshi", 10.0d);
+        sistemaNotasAlunos.put("Santiago", 7.5d);
+        sistemaNotasAlunos.put("Luisa", 8.9d);
+    }
+}
