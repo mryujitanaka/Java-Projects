0e1cdfcc3c0ec807a9dc18bcdfda1f379191ac66
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-09-13 11:16:21 -0300

    Create Aula-38

--- JavanautaAcademy/Aula-38/Casa/src/PlantaCasa.java
+++ JavanautaAcademy/Aula-38/Casa/src/PlantaCasa.java
@@ -1,5 +1,20 @@
 public class PlantaCasa {
-    public static void main(String[] args) {
-        System.out.println("Test");
+    // Atributos / Variáveis / Características
+    int metragemCasa;
+    int numeroQuartosCasa;
+    int numeroBanheirosCasa;
+    String tipoMaterialCasa;
+    String corCasa;
+
+    // Método ou Função (ordens)
+    public void Construir(){
+        System.out.println("Metragem: " + metragemCasa);
+        System.out.println("Número de quartos: : " + numeroQuartosCasa);
+        System.out.println("Número de banheiros: " + numeroBanheirosCasa);
+        System.out.println("Tipo de Material: " + tipoMaterialCasa);
+    }
+    // Método ou Função (ordem)
+    public void Pintar(){
+        System.out.println("Cor: " + corCasa);
     }
 }
